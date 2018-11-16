3b098e987c86ea0de1e956a76f2209a4c0a3928f
mshPolo <marcus1x@live.dk>
2018-11-16 17:06:58 +0100

    removed spaghetti

--- pommerman/util.py
+++ pommerman/util.py
@@ -102,3 +102,42 @@ def get_valid_actions(obs):
         valid_actions.append(Action.Bomb)
     return valid_actions
 
+
+def flatten_state_not_first_board(s):
+    # Usage Example:
+	# def forward(self, x):
+	#     x = flatten_state(x)
+	# where x is np.atleast1d(S[0])
+	return torch.from_numpy(np.array([flatten_state_aux_not_first_board(x) for x in s])).float()
+	
+
+def flatten_state_aux_not_first_board(s):
+    # Lists
+    alive = [1 if x in s['alive'] else 0 for x in range(10,14)]
+    #board = s['board']
+    bomb_blast_strength = s['bomb_blast_strength']
+    bomb_life = s['bomb_life']
+    # Tuples
+    position = s['position']
+    # Ints
+    blast_strength = s['blast_strength']
+    can_kick = s['can_kick']
+    ammo = s['ammo']
+    # Enums
+    teammate = s['teammate'] #9 for FFA
+    enemies = s['enemies'] #11,12,13 for FFA and training agent id = 0
+    
+    a = np.array(alive)
+    #a = np.append(np.array(alive),np.array(board).flatten())
+    a = np.append(a,np.array(bomb_blast_strength).flatten())
+    a = np.append(a,np.array(bomb_life).flatten())
+    a = np.append(a,position[0])
+    a = np.append(a,position[1])
+    a = np.append(a,blast_strength)
+    a = np.append(a,can_kick)
+    a = np.append(a,ammo)
+    # Commented out as we get size 376 but expected 372. I assume we calculated wrong.
+    # Makes sense to ignore these imo
+    #a = np.append(a,teammate.value)
+    #a = np.append(a,[e.value for e in enemies])
+    return a.astype(float)
