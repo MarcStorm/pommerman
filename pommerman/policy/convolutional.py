e3bf6a67085d001e83ad770f022b7f6e0241f12f
tympanix <tympanix@gmail.com>
2018-11-19 14:02:01 +0100

    refactor: policy module

--- pommerman/policy/convolutional.py
+++ pommerman/policy/convolutional.py
@@ -1,59 +1,11 @@
 """
-Module implements police networks for the REINFORCE algorithm
+Module implements a convolutional neural network
 """
+
 import torch
 from torch import nn, optim, F
-from util import get_cuda, get_numpy
+from policy import PolicyNet, get_cuda, get_numpy
 import numpy as np
-
-
-class PolicyNet(nn.Module):
-    """Policy network"""
-
-    def loss(self, action_probabilities, returns):
-        return -torch.mean(torch.mul(torch.log(action_probabilities), returns))
-
-
-    def enemy_map(self, obs):
-        """
-        Returns an 11x11 ndarray where 1 indicated enemy
-        and 0 indicates no enemy
-        """
-        board = obs['board'].copy()
-        enemies = obs['enemies']
-        mask = np.isin(board, enemies)
-        board[mask] = 1
-        board[~mask] = 0
-        return board
-
-
-    def position_map(self, obs):
-        """
-        Returns an 11x11 ndarray with a single value set to 1
-        indicating the agents position. Other values are 0
-        """
-        board = np.zeros_like(obs['board'])
-        pos = obs['position']
-        board[pos[0], pos[1]] = 1
-        return board    
-
-
-    def danger_map(self, obs):
-        """
-        Returns an 11x11 ndarray where 0 indicates no danger
-        and >0 indicates some danger level (typically bombs).
-        Greater numbers means more immediate danger
-        """
-        board = np.zeros_like(obs['board'])
-        strength = obs['board_blast_strength']
-        life = obs['bomb_life']
-        for x,y in np.argwhere(life>0):
-            s = strength[x,y]
-            l = life[x,y]
-            board[y,max(0, x-s):min(10, x+s)] = l
-            board[max(0, y-s):min(10, y+s),x] = l
-        return board
-
 
 
 class ConvNet(PolicyNet):
