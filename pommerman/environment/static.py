4319cfe99743d907a300fbc316d56711506cf236
tympanix <tympanix@gmail.com>
2018-11-30 14:02:06 +0100

    feat: static board

--- pommerman/environment/static.py
+++ pommerman/environment/static.py
@@ -1,28 +1,31 @@
 from pommerman.envs.v0 import Pomme
-
+import os
 import json
 
-STATE_FILE = './pommerman.json'
+PATH = os.path.dirname(os.path.realpath(__file__))
+
+STATE_FILE = os.path.join(PATH, './pommerman.board.json')
 
 class StaticPomme(Pomme):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.set_init_game_state(STATE_FILE)
         self.is_locked = False
 
     
     def reset(self):
-        obs = super().reset()
         if not self.is_locked:
-            self.lock_game_state()
-        return obs
+            self.set_init_game_state(STATE_FILE)
+            self.is_locked = True
+        return super().reset()
 
 
-    def lock_game_state(self):
-        with open(STATE_FILE, 'w') as f:
-            info = self.get_json_info()
-            f.write(json.dumps(info, sort_keys=True, indent=4))
-        self.set_init_game_state(STATE_FILE)
-        self.is_locked = True
+    # def lock_game_state(self):
+    #     with open(STATE_FILE, 'w') as f:
+    #         info = self.get_json_info()
+    #         f.write(json.dumps(info, sort_keys=True, indent=4))
+    #     self.set_init_game_state(STATE_FILE)
+    #     self.is_locked = True
 
     
