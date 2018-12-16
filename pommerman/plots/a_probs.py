0066ebfa4c0fdd691c5b0a710f3fba72a4651f24
tympanix <tympanix@gmail.com>
2018-12-16 14:27:14 +0100

    add: plots

---
+++ pommerman/plots/a_probs.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+from matplotlib.figure import Figure
+import torch.nn.functional as F
+import math
+import numpy as np
+import json
+
+plt.style.use('seaborn-darkgrid')
+
+f = open('a_probs_list_softmax.json', 'r')
+data = json.loads(f.readline())
+
+fig = plt.figure()
+colors = ['green', 'blue', 'purple', 'orange', 'red', 'cyan']
+labels = ['Stop', 'Up', 'Down', 'Left', 'Right', 'Bomb']
+for l, c, i in zip(labels, colors, range(0, 6)):
+    x = [math.floor(i*16.6667) for i in range(0, len(data))]
+    y = [d[i] for d in data]
+    plt.plot(x, y, color=c, label=l)
+
+plt.legend(loc='upper left')
+plt.xlabel('Iterations')
+plt.ylabel('Probability')
+plt.show()
+
+fig.savefig("a_probs.pdf", bbox_inches='tight')
---
+++ pommerman/plots/random_train_val.py
@@ -0,0 +1,41 @@
+import matplotlib.pyplot as plt
+from matplotlib.figure import Figure
+import torch.nn.functional as F
+import math
+import numpy as np
+import json
+
+
+def movingaverage(values, window):
+    weights = np.repeat(1.0, window)/window
+    sma = np.convolve(values, weights, 'valid')
+    return sma
+
+
+plt.style.use('seaborn-darkgrid')
+
+f = open('random_training_rewards_softmax.json', 'r')
+train = json.loads(f.readline())
+f.close()
+
+f = open('random_validation_rewards_softmax.json', 'r')
+val = json.loads(f.readline())
+f.close()
+
+print(len(train))
+print(len(val))
+
+
+fig = plt.figure()
+y = movingaverage(train, 200)
+plt.plot(range(0, len(y)), y, color='blue', label='Training')
+n = 50
+y = [sum(val[i:i + n])/50 for i in range(0, len(val), n)]
+plt.plot([i*250 for i in range(0, len(y))], y, color='green', label='Validation')
+
+plt.legend(loc='lower right')
+plt.xlabel('Iterations')
+plt.ylabel('Reward')
+plt.show()
+
+fig.savefig("random_train_val.pdf", bbox_inches='tight')
