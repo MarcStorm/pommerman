abbbd2d3634403d2cc4e4f7595ab5f590a7c5228
tympanix <tympanix@gmail.com>
2018-12-17 14:16:14 +0100

    add: epsilon plots for 20 and 8

--- pommerman/plots/epsilon.py
+++ pommerman/plots/epsilon.py
@@ -10,16 +10,17 @@ num_episodes = 150000
 epsilon = 1.0
 for i in range(num_episodes):
     data.append(epsilon)
-    epsilon *= num_episodes / (i / (num_episodes / 20) + num_episodes)  # decrease epsilon
+    epsilon *= num_episodes / (i / (num_episodes / 8) + num_episodes)  # decrease epsilon
 
 
 w, h = plt.figaspect(0.35)
-fig = plt.figure(figsize=(w,h))
+fig = plt.figure(figsize=(w*0.5,h*0.5))
 plt.plot(range(num_episodes), data, color='blue', label='Epsilon')
 
 plt.legend(loc='lower left')
 plt.xlabel('Iterations')
 plt.ylabel('Epsilon')
+plt.xticks([])
 plt.show()
 
-fig.savefig("epsilon_20.pdf", bbox_inches='tight', transparent=True)
+fig.savefig("epsilon_8.pdf", bbox_inches='tight', transparent=True)
